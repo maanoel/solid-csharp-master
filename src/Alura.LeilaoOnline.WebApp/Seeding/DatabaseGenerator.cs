b976626d0862abb1bea949cc3db37d22bdec7aab
maanoel <manoelvitorbrito@gmail.com>
2021-11-25 22:35:14 -0300

    Alterações gerais do SOLID

--- src/Alura.LeilaoOnline.WebApp/Seeding/DatabaseGenerator.cs
+++ src/Alura.LeilaoOnline.WebApp/Seeding/DatabaseGenerator.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Collections.Generic;
 using Alura.LeilaoOnline.WebApp.Models;
-using Alura.LeilaoOnline.WebApp.Dados;
+using Alura.LeilaoOnline.WebApp.Dados.EfCore;
 
 namespace Alura.LeilaoOnline.WebApp.Seeding
 {
-	public static class DatabaseGenerator
+    public static class DatabaseGenerator
     {
         public static void Seed()
         {
